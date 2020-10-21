72b7f8fcffdf67c69d8e927dd4489d51d4b85e2d
neurotrino <neurotrino@gmail.com>
2020-10-21 18:54:37 -0500

    increase input w

--- models.py
+++ models.py
@@ -584,7 +584,7 @@ class Adex_EI(tf.keras.layers.Layer):
                                              name='input_weights')
         '''
         self.input_weights = self.add_weight(shape=(input_shape[-1], self.units),
-                                             initializer=tf.keras.initializers.RandomUniform(minval=0., maxval=1),
+                                             initializer=tf.keras.initializers.RandomUniform(minval=0., maxval=2),
                                              trainable = True,
                                              name='input_weights')
         # Create the recurrent weights, their value here is not important
