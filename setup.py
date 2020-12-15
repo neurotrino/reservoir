76e3fa21f089ead532c952f16efaa3db1da208ca
Chadbourne Smith <smith7@uchicago.edu>
2020-12-14 23:20:57 -0600

    Rename package

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 
 # Setup
 setup(
-    name='mssn',
+    name='maclean-snn',
     description='First SNN infrastructure from the MacLean group',
     version='2.0',
     packages=find_packages(),
