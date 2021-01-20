28ae1fb93cd6baf21fb8276d270f65538090c1fe
neurotrino <neurotrino@gmail.com>
2021-01-20 14:40:57 -0700

    uncertain of attributes and dimensions needed for regularization definitions

--- models/common.py
+++ models/common.py
@@ -59,31 +59,63 @@ class SynchronyRegularization(tf.keras.layers.Layer):
         spike = inputs[1]
 
         synchrony = fano_factor(self, spike)
-        self.add_metric(synchrony, name='synchrony', aggregation='mean')
+        global_synchrony = tf.reduce_mean(synchrony)
+        self.add_metric(global_synchrony, name='synchrony', aggregation='mean')
 
         reg_loss = tf.reduce_sum(tf.square(synchrony - self._target_synch)) * self._synch_cost
         self.add_loss(reg_loss)
         self.add_metric(reg_loss, name='synch_loss', aggregation='mean')
 
         return inputs
-"""
+
 
 def fano_factor(self, spike):
-    """
-    Calculate value similar to the Fano factor to estimate synchrony quickly
-    During each bin, calculate the variance of the number of spikes per neuron divided by the mean of the number of spikes per neuron
-    The Fano factor during one interval is equal to the mean of the values calculated for each bin in it
-    """
+
+    #Calculate value similar to the Fano factor to estimate synchrony quickly
+    #During each bin, calculate the variance of the number of spikes per neuron divided by the mean of the number of spikes per neuron
+    #The Fano factor during one interval is equal to the mean of the values calculated for each bin in it
+    #Spike should have dims of trial, neuron, time
+    #Returned fano factor should have dims of trial
+
+
+    # leaving this unfinished for now as accessible attributes and dimensions are unclear 
+
+
     try:
-        n_fano = 0
+        len_bins = 10
+        n_fano = [0]*self.num_intervals
+        len_interval = self.last_spiked/self.num_intervals
+        n_bins = int(round(len_interval/len_bins))
+        for m in range(0, self.num_intervals):
+            fano_all = [0] * n_bins
+            print(len_interval*m)
+            for i in range(0, n_bins):
+                spikes_per_neuron = [0]*(self.n_neurons-self.n_inhib)
+                for j in range(0, self.n_neurons-self.n_inhib):
+                    for k in self.spike_trains[j]:
+                        if len_interval*m + len_bins*i <= k < len_interval*m + len_bins*(i+1):
+                            spikes_per_neuron[j] += 1
+                fano_all[i] = var(spikes_per_neuron)/mean(spikes_per_neuron)
+            n_fano[m] = mean(fano_all)
+        return n_fano
+
+
+    try:
+        len_bins = 10
+        n_fano = [0]*(self.seq_len/len_bins)
+        len_interval = self.seq_len/len_bins
+        n_bins = int(round(len_interval/len_bins))
         fano_all = [0]*(self._cell.units)
             for i in range(0, self._cell.units):
                 fano_all[i] = var(spike[i])/mean(spike[i])
         n_fano = mean(fano_all)
     return n_fano
+
     except Exception as e:
         print(e)
         return False
+
+"""
 
 class SpikeRegularization(tf.keras.layers.Layer):
     """TODO: docs"""
@@ -95,7 +127,14 @@ class SpikeRegularization(tf.keras.layers.Layer):
         self._cell = cell
 
     def call(self, inputs, **kwargs):
+
+        # it is unclear to me what self refers to here. is it cell? if so, does it
+        # have the attributes of model: cell: in hjson? some of which conflict with/are redundant with uni?
+        # for example, if I want to access the duration of a trial, am I able to call self.seq_len since
+        # that is under uni, or is it not accessible?
+
         """TODO: docs"""
+        # inputs should be in dims of trial (i.e. batch size) x neuron x time
         voltage = inputs[0]
         spike = inputs[1]
         upper_threshold = self._cell.thr
@@ -104,14 +143,14 @@ class SpikeRegularization(tf.keras.layers.Layer):
         # av = Second * tf.reduce_mean(z, axis=(0, 1)) / flags.dt
         #regularization_coeff = tf.Variable(np.ones(flags.n_neurons) * flags.reg_fr, dtype=tf.float32, trainable=False)
         #loss_reg_fr = tf.reduce_sum(tf.square(rate - flags.target_rate) * regularization_coeff)
-        global_rate = tf.reduce_mean(rate)
+        global_rate = tf.reduce_mean(rate) # I think this is for the whole batch of trials?
         self.add_metric(global_rate, name='rate', aggregation='mean')
 
         reg_loss = tf.reduce_sum(tf.square(rate - self._target_rate)) * self._rate_cost
         self.add_loss(reg_loss)
         self.add_metric(reg_loss, name='rate_loss', aggregation='mean')
 
-        return inputs
+        return inputs # unchanged; facets of self however are changed
 
 class SpikeVoltageRegularization(tf.keras.layers.Layer):
     """TODO: docs"""
