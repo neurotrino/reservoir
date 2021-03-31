e04ad555b4bef2f80a2ff0f30e7439d289a9daf1
neurotrino <neurotrino@gmail.com>
2021-03-31 10:09:03 -0500

    revert tfp

--- models/common.py
+++ models/common.py
@@ -4,7 +4,7 @@ from typing import Any
 
 import numpy as np
 import tensorflow as tf
-import tensorflow-probability as tfp
+import tensorflow_probability as tfp
 
 #┬───────────────────────────────────────────────────────────────────────────╮
 #┤ Base (Inherited) Model                                                    │
