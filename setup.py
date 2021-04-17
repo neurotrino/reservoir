edd4d77c6cb0a3b1f2f7833d6c4a8221d87ae2f0
Chadbourne Smith <smith7@uchicago.edu>
2021-04-16 20:08:58 -0500

    Add persistent environmental variables

--- setup.py
+++ setup.py
@@ -5,8 +5,52 @@ Run `python setup.py develop` to install dependencies.
 
 from setuptools import setup, find_packages
 
+import os
+import subprocess
+
+
+def installation_wrapper(setup):
+    """Code to be run when the setup script is invoked (i.e. when we
+    call `pip install -e .`).
+    """
+
+    # Detect environment
+    try:
+        # Conda
+        venv_path = os.path.join(os.environ['CONDA_PREFIX'], 'lib')
+    except:
+        # Standard venv
+        venv_path = os.path.join(os.environ['VIRTUAL_ENV'], 'lib')
+
+    # Find the directory we need to create our .pth file in
+    for path, dirs, _ in os.walk(venv_path):
+        if 'site-packages' in dirs:
+            venv_path = os.path.join(path, 'site-packages')
+
+    # Bake gitsha into the venv
+    sha = subprocess.check_output(["git", "rev-parse", "HEAD"]).strip()
+    sha = sha.decode('utf-8')  # returned as bytes, so we decode
+
+    # Extensible
+    evars = [  # (name, value)
+        ('GITSHA', sha),
+    ]
+
+    # Creat environment variables
+    with open(os.path.join(venv_path, 'mssn-custom-evars.pth'), 'w+') as file:
+
+        pth_txt = "import os;"
+
+        for (k, v) in evars:
+            pth_txt += f"os.environ['MSNN_{k}']='{v}';"
+
+        file.write(pth_txt)
+
+    return setup
+
+
 # Setup
-setup(
+setup = installation_wrapper(setup(
     name='msnn',
     description='An SNN infrastructure from the MacLean group',
     version='2.0',
@@ -17,4 +61,4 @@ setup(
         'matplotlib',
         'tensorflow'
     ]
-)
+))
