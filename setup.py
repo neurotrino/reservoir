a3449099007f5e61e4474de63292f5f5a4c82da8
neurotrino <neurotrino@gmail.com>
2021-07-27 08:20:19 -0500

    remove version specification for tensorflow in setup.py'

--- setup.py
+++ setup.py
@@ -73,7 +73,8 @@ setup = installation_wrapper(setup(
         'dataclasses_json',
         'hjson',
         'matplotlib',
-        'tensorflow' >= 2.5,
+        #'tensorflow' >= 2.5,
+        'tensorflow'
         'tensorflow-probability'
     ]
 ))
