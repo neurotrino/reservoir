226c399717a92b840a04561c5f578f54759fc6b7
Chadbourne Smith <smith7@uchicago.edu>
2021-08-14 18:51:38 -0500

    Update version label

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ def installation_wrapper(setup):
 setup = installation_wrapper(setup(
     name='msnn',
     description='An SNN infrastructure from the MacLean group',
-    version='2.0',
+    version='2.1.0',
     packages=find_packages(),
     install_requires=[
         'dataclasses_json',
