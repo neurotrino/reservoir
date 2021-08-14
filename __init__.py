678741109d82e114bc591e7784f211371ed882d0
Chadbourne Smith <smith7@uchicago.edu>
2021-08-14 16:44:31 -0500

    Import fixes

--- __init__.py
+++ __init__.py
@@ -1,10 +1,14 @@
-from os.path import basename, dirname, isfile, join
-import glob
-modules = glob.glob(join(dirname(__file__), "*.py"))
-__all__ = [
-    basename(f)[:-3]
-        for f
-        in modules
-        if isfile(f) and not f.endswith('__init__.py')
-]
-from . import *
+import pkgutil
+import sys
+
+
+def load_all_modules_from_dir(dirname):
+    for importer, package_name, _ in pkgutil.iter_modules([dirname]):
+        full_package_name = '%s.%s' % (dirname, package_name)
+        if full_package_name not in sys.modules:
+            module = importer.find_module(package_name
+                        ).load_module(full_package_name)
+            print module
+
+
+load_all_modules_from_dir('Foo')
