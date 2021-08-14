162475999835a67ab4819dd6412b7d6a22b0f815
Chadbourne Smith <smith7@uchicago.edu>
2021-08-14 16:58:26 -0500

    Import fixes

---
+++ __init__.py
@@ -0,0 +1,8 @@
+import os
+import importlib
+
+__globals = globals()
+
+for file in os.listdir(os.path.dirname(__file__)):
+    mod_name = file[:-3]   # strip .py at the end
+    __globals[mod_name] = importlib.import_module('.' + mod_name, package=__name__)
