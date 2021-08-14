dde4ff9072085a3e9d70b50e7b39e9af517e20b7
Chadbourne Smith <smith7@uchicago.edu>
2021-08-14 16:37:09 -0500

    Import fixes

--- main.py
+++ main.py
@@ -7,7 +7,23 @@ import os
 import tensorflow as tf
 import utils.config
 
-from . import *
+from os.path import dirname, basename, isfile, join
+import glob
+modules = glob.glob(join(dirname(__file__), "*.py"))
+__all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
+
+
+# Build model ----
+from models import *
+
+# Load Data ------
+from data import *
+
+# Log ------------
+from loggers import *
+
+# Train ----------
+from trainers import *
 
 def main():
     # Use command line arguments to load data, create directories, etc.
