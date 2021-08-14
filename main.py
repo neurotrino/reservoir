7f7a20d42582bc77dfb3b568be078f21343dea78
Chadbourne Smith <smith7@uchicago.edu>
2021-08-14 18:05:42 -0500

    Add docs

--- main.py
+++ main.py
@@ -1,33 +1,43 @@
-"""Most commonly used script to run models from."""
-
 from os.path import abspath
-
-import logging
-import os
-import tensorflow as tf
-import utils.config
-
-
 from pathlib import Path
 
 import importlib.util
+import logging
+import utils.config
 
-#=================================
+#======================================================================
+# What is the below block of code? I'm sorry you asked. Basically,
+# Python has a trash import system, so we need to hard code our import
+# logic below in order to implement adequate abstraction in `main.py`.
+#
+# Functionally, I recommend ignoring the witchcraft below and just
+# know that what's basically happening is submodules are getting
+# registered by Python as things like `models.eprop_sinusoid_ex` or
+# `data.ccd`. It's as though we had manually called `import data.ccd`
+# and so on, just without the "manual" part.
+#
+# Why do we want this? It allows us to specify in our HJSON files the
+# modules we want to use per experiment. We were already doing this,
+# but now...
+# 1. we only need to change class names in one place (the HJSON file)
+# 2. we can run this script outside of virtual environments, meaning we
+#    don't have to worry about weird `pip install -e .` behaviors
+# 3. we only need one `main.py` file, and we never have to touch it
+#----------------------------------------------------------------------
 for filepath in Path('.').rglob('*.py'):
     fp_str = str(filepath)
 
     if fp_str not in [__file__, "__init__.py", "setup.py"]:
-        # Convert filepath syntax to modulepath syntax
+        # Convert filepaths to module paths
         module_name = fp_str[:-3]
         module_name = module_name.replace('/', '.')
 
         # Load module into python
         spec = importlib.util.spec_from_file_location(module_name, fp_str)
-        print()
-        print(fp_str)
-        print()
-        spec.loader.exec_module(importlib.util.module_from_spec(spec))  # error-causing line
-#=================================
+        spec.loader.exec_module(importlib.util.module_from_spec(spec))
+
+        logging.debug(f"registered {fp_str}")
+#======================================================================
 
 def main():
     # Use command line arguments to load data, create directories, etc.
