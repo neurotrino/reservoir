60e2439a4779cda46f126c0f00e7900605f48ed9
Chadbourne Smith <smith7@uchicago.edu>
2021-08-14 16:27:18 -0500

    Syntax fixes

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def main():
     logging.info("experiment directory: " + abspath(cfg['save'].exp_dir))
 
     # Build model
-    model = eval(f"models.{cfg['model'].type}")(cfg)
+    model = eval(f"models.grey_goo.{cfg['model'].type}")(cfg)
     logging.info(f"model built: {cfg['model'].type}")
 
     # Load data
