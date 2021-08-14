3a7f13bc89186b32627620b951ccd186b19a95e2
Chadbourne Smith <smith7@uchicago.edu>
2021-08-14 16:05:08 -0500

    Syntax fixes

--- main.py
+++ main.py
@@ -25,19 +25,21 @@ def main():
     logging.info("experiment directory: " + abspath(cfg['save'].exp_dir))
 
     # Build model
-    model = eval(cfg['model'].type).Model(cfg)
+    model = eval(f'models.{cfg['model'].type}').Model(cfg)
     logging.info(f"model built: {cfg['model'].type}")
 
     # Load data
-    data = eval(cfg['data'].type).DataGenerator(cfg)
+    data = eval(f'data.{cfg['data'].type}').DataGenerator(cfg)
     logging.info(f"dataset loaded: {cfg['data'].type}")
 
     # Instantiate logger
-    logger = eval(cfg['log'].type).Logger(cfg)
+    logger = eval(f'loggers.{cfg['log'].type}').Logger(cfg)
     logging.info(f"logger instantiated: {cfg['log'].type}")
 
     # Instantiate trainer
-    trainer = eval(cfg['train'].type).Trainer(cfg, model, data, logger)
+    trainer = eval(f'trainers.{cfg['train'].type}').Trainer(
+        cfg, model, data, logger
+    )
     logging.info(f"trainer instantiated: {cfg['train'].type}")
 
     # Train model
