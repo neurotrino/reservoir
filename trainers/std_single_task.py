98461e68ff9e7b3bfbb668fc9dfaeb2d07569668
Chadbourne Smith <smith7@uchicago.edu>
2021-08-26 12:42:10 -0500

    Update std_single_task.py

--- trainers/std_single_task.py
+++ trainers/std_single_task.py
@@ -279,7 +279,7 @@ class Trainer(BaseTrainer):
                 print(meta_indices)
                 print()
                 print()
-                zero_indices = zero_indices[[tf.cast(x, tf.int32) for x in meta_indices]]  # [!] would prefer not to have to cast
+                zero_indices = zero_indices[meta_indices]  # [!] would prefer not to have to cast
                 print()
                 print('C:')
                 print(zero_indices)
