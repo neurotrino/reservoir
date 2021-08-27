e28c06ac7e85a6787caf021e62ba2f5706111bb7
Chadbourne Smith <smith7@uchicago.edu>
2021-08-27 01:18:27 -0500

    Syntax fix

--- models/neurons/lif.py
+++ models/neurons/lif.py
@@ -42,7 +42,7 @@ class LIF(Neuron):
     #┴───────────────────────────────────────────────────────────────────────╯
 
     def __init__(self, cfg):
-        super().__init__(self, cfg)
+        Neuron.__init__(self, cfg)
 
         self.cfg = cfg
         cell_cfg = cfg['cell']
