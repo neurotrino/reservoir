1c9c6426048d4f8988c1579a3fede82db3c84a6c
Chadbourne Smith <smith7@uchicago.edu>
2022-06-07 12:10:21 -0500

    Add control for initial voltage values (#112)

---
+++ utils/stats.py
@@ -0,0 +1,32 @@
+"""Statistical tools."""
+
+import tensorflow as tf
+
+class StatisticalDistribution:
+    """Generic function wrapper aliasing statistical distributions."""
+
+    def __init__(self, sampling_fn, **kwargs) -> None:
+
+        @tf.function
+        def wrapped_fn(shape: tuple[int, ...], **calltime_kwargs):
+            """
+            Preconfigured sampling function capable of taking further
+            arguments at calltime.
+            """
+            joint_kwargs = kwargs | calltime_kwargs
+            return sampling_fn(shape, **joint_kwargs)
+
+
+        self._sampling_fn = wrapped_fn
+
+
+    @tf.function
+    def sample(self, shape: tuple[int, ...], **calltime_kwargs):
+        """Sample from the configured distribution.
+
+        Arguments:
+            shape: shape of samples to return.
+            calltime_kwargs: arguments which must be given to the
+                sampling function at call time.
+        """
+        return self._sampling_fn(shape, **calltime_kwargs)
