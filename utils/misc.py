3643d5673a72cbc86956370e01b319d48e7fe785
Chadbourne Smith <smith7@uchicago.edu>
2022-06-16 11:58:12 -0500

    Move `filenames()` to `utils.misc`

--- utils/misc.py
+++ utils/misc.py
@@ -28,3 +28,16 @@ class SwitchedDecorator:
                 f"{type(new_val)}"
             )
         self._enabled = new_val
+
+
+def filenames(num_epochs, epochs_per_file):
+    """Get the filenames storing data for epoch ranges.
+    Our data is stored in npz files titled 'x-y.npz' indicating that
+    file contains the data for epochs x through y, inclusive. For
+    example, 1-10.npz has all the data associated with the first 10
+    epochs of an experiment.
+    """
+    return [
+        f"{i}-{i + epochs_per_file - 1}.npz"
+        for i in range(1, num_epochs, epochs_per_file)
+    ]
