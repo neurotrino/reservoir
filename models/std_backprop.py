a8adea8d3781a0551eec8108c1f6990e16fa37ec
Chadbourne Smith <smith7@uchicago.edu>
2022-06-16 13:51:56 -0500

    Update std_backprop.py

--- models/std_backprop.py
+++ models/std_backprop.py
@@ -54,12 +54,13 @@ class Model(BaseModel):
 
     @switched_tf_function
     def noise_weights(self, mean=1.0, stddev=0.1):
-        print(len(self.rnn1.get_weights()))
+        print(self.rnn1.get_weights()[0].shape)
+        print(self.rnn1.get_weights()[1].shape)
         gain_matrix = tf.random.normal(
             self.rnn1.get_weights()[0].shape, mean, stddev
         )
         noised_weights = self.rnn1.get_weights()[0] * gain_matrix
-        self.rnn1.set_weights(noised_weights)
+        self.rnn1.set_weights([self.rnn1.ge_weights()[0], noised_weights])
 
 
     @switched_tf_function
