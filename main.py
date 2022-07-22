4b354154c23936c759259247890590475640bfea
Chadbourne Smith <smith7@uchicago.edu>
2022-07-22 13:35:09 -0500

    Loop main()

--- main.py
+++ main.py
@@ -39,28 +39,29 @@ def main():
     logging.info("experiment directory: " + abspath(cfg['save'].exp_dir))
 
     # Build model
-    model_module = eval(f"models.{cfg['model'].type}")
-    model = model_module.Model(cfg)
-    logging.info(f"instantiated {cfg['model'].type}.Model")
+    while True:
+        model_module = eval(f"models.{cfg['model'].type}")
+        model = model_module.Model(cfg)
+        logging.info(f"instantiated {cfg['model'].type}.Model")
 
-    # Load data
-    data_module = eval(f"data.{cfg['data'].type}")
-    data = data_module.DataGenerator(cfg)
-    logging.info(f"instantiated {cfg['data'].type}.DataGenerator")
+        # Load data
+        data_module = eval(f"data.{cfg['data'].type}")
+        data = data_module.DataGenerator(cfg)
+        logging.info(f"instantiated {cfg['data'].type}.DataGenerator")
 
-    # Instantiate logger
-    logger_module = eval(f"loggers.{cfg['log'].type}")
-    logger = logger_module.Logger(cfg)
-    logging.info(f"instantiated {cfg['log'].type}.Logger")
+        # Instantiate logger
+        logger_module = eval(f"loggers.{cfg['log'].type}")
+        logger = logger_module.Logger(cfg)
+        logging.info(f"instantiated {cfg['log'].type}.Logger")
 
-    # Instantiate trainer
-    trainer_module = eval(f"trainers.{cfg['train'].type}")
-    trainer = trainer_module.Trainer(cfg, model, data, logger)
-    logging.info(f"instantiated {cfg['train'].type}.Trainer")
+        # Instantiate trainer
+        trainer_module = eval(f"trainers.{cfg['train'].type}")
+        trainer = trainer_module.Trainer(cfg, model, data, logger)
+        logging.info(f"instantiated {cfg['train'].type}.Trainer")
 
-    # Train model
-    trainer.train()
-    logging.info("training complete")
+        # Train model
+        trainer.train()
+        logging.info("training complete")
 
 
 if __name__ == '__main__':
