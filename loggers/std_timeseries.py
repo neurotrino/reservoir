8cb43bf12559ec3a4edd2645a329bdd20282ae79
neurotrino <neurotrino@gmail.com>
2022-07-28 13:51:27 -0500

    remove plot_everything for now

--- loggers/std_timeseries.py
+++ loggers/std_timeseries.py
@@ -257,7 +257,7 @@ class Logger(BaseLogger):
         hi_epoch = self.cur_epoch
         for epoch_idx in range(self.cfg['log'].post_every):
             step_idx = epoch_idx * self.cfg['train'].n_batch
-            self.plot_everything(f"{lo_epoch + epoch_idx}", step_idx)
+            #self.plot_everything(f"{lo_epoch + epoch_idx}", step_idx)
 
         # Write to disk, free RAM, and perform bookkeeping
         super().post()
