6cff0e270991b41878bbf387c9fd4d11958e1985
neurotrino <neurotrino@gmail.com>
2022-08-02 12:27:18 -0500

    smaller bin widths

--- utils/extools/analyze_structure.py
+++ utils/extools/analyze_structure.py
@@ -418,9 +418,9 @@ def plot_output_w_dist_experiments():
             e_to_out = w[i][0:e_end,:]
             i_to_out = w[i][e_end:i_end,:]
             # plot nonzero e-to-output
-            sns.histplot(data=np.ravel(e_to_out[e_to_out!=0]), binwidth=0.1, color='blue', label='from e units', stat='density', alpha=0.5, kde=True, edgecolor='white', linewidth=0.5, line_kws=dict(color='black', alpha=0.5, linewidth=1.5))
+            sns.histplot(data=np.ravel(e_to_out[e_to_out!=0]), binwidth=0.05, color='blue', label='from e units', stat='density', alpha=0.5, kde=True, edgecolor='white', linewidth=0.5, line_kws=dict(color='black', alpha=0.5, linewidth=1.5))
             # plot nonzero i-to-output
-            sns.histplot(data=np.ravel(i_to_out[i_to_out!=0]), binwidth=0.1, color='red', label='from i units', stat='density', alpha=0.5, kde=True, edgecolor='white', linewidth=0.5, line_kws=dict(color='black', alpha=0.5, linewidth=1.5))
+            sns.histplot(data=np.ravel(i_to_out[i_to_out!=0]), binwidth=0.05, color='red', label='from i units', stat='density', alpha=0.5, kde=True, edgecolor='white', linewidth=0.5, line_kws=dict(color='black', alpha=0.5, linewidth=1.5))
             plt.xlabel('nonzero output weight distribution')
             plt.ylabel('density')
             plt.title(plt_string[i])
