b1d176221af4970e6b5a7a48f9992b1a03f90835
neurotrino <neurotrino@gmail.com>
2022-08-04 15:40:00 -0500

    information centrality for fun

--- utils/extools/analyze_structure.py
+++ utils/extools/analyze_structure.py
@@ -56,23 +56,23 @@ def plot_eigvc_dist_experiments():
             Ge = nx.from_numpy_array(w[i][0:e_end,0:e_end],create_using=nx.DiGraph)
             Gi = nx.from_numpy_array(np.abs(w[i][e_end:i_end,e_end:i_end]),create_using=nx.DiGraph)
             # plot centrality between e units
-            result = list(nx.eigenvector_centrality_numpy(Ge,weight='weight').items())
+            result = list(nx.information_centrality(Ge,weight='weight').items())
             e_eigvc = np.array(result)[:,1]
             sns.histplot(data=np.ravel(e_eigvc), bins=30, color='blue', label='within e units', stat='density', alpha=0.5, kde=True, edgecolor='white', linewidth=0.5, line_kws=dict(color='black', alpha=0.5, linewidth=1.5))
             # plot centrality between i units
-            result = list(nx.eigenvector_centrality_numpy(Gi,weight='weight').items())
+            result = list(nx.information_centrality(Gi,weight='weight').items())
             i_eigvc = np.array(result)[:,1]
             sns.histplot(data=np.ravel(i_eigvc), bins=30, color='red', label='within i units', stat='density', alpha=0.5, kde=True, edgecolor='white', linewidth=0.5, line_kws=dict(color='black', alpha=0.5, linewidth=1.5))
             # plot whole network clustering
             #result = list(nx.clustering(G,nodes=G.nodes,weight='weight').items())
             #cc = np.array(result)[:,1]
             #sns.histplot(data=np.ravel(cc), bins=30, color='black', label='whole network', stat='density', alpha=0.5, kde=True, edgecolor='white', linewidth=0.5, line_kws=dict(color='black', alpha=0.5, linewidth=1.5))
-            plt.xlabel('weighted eigenvector centrality for recurrent layer')
+            plt.xlabel('absolute weighted information centrality for recurrent layer')
             plt.ylabel('density')
             plt.title(plt_string[i])
             plt.legend()
             plt.draw()
-            plt_name = plt_string[i]+"_eigvc_dist_exp.png"
+            plt_name = plt_string[i]+"_infc_dist_exp.png"
             plt.savefig(os.path.join(savepath,exp_path,plt_name),dpi=300)
             plt.clf()
             plt.close()
