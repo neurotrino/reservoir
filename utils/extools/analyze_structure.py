3d218e388406dc8230de0df7927164d970eb27dc
neurotrino <neurotrino@gmail.com>
2022-08-04 14:28:50 -0500

    plot clustering distributions

--- utils/extools/analyze_structure.py
+++ utils/extools/analyze_structure.py
@@ -27,6 +27,50 @@ e_end = 240
 i_end = 300
 savepath = '/data/results/experiment1/'
 
+def plot_clustering_dist_experiments():
+    experiments = get_experiments(data_dir, experiment_string)
+    plt_string = ['epoch0','epoch10','epoch100','epoch1000']
+
+    for xdir in experiments:
+        # create experiment-specific folder for saving if it doesn't exist yet
+        exp_path = xdir[-9:-1]
+        if not os.path.isdir(os.path.join(savepath,exp_path)):
+            os.makedirs(os.path.join(savepath,exp_path))
+
+        data_files = []
+        data_files.append(os.path.join(data_dir, xdir, 'npz-data/main_preweights.npy'))
+        data_files.append(os.path.join(data_dir, xdir, 'npz-data/1-10.npz'))
+        data_files.append(os.path.join(data_dir, xdir, 'npz-data/91-100.npz'))
+        data_files.append(os.path.join(data_dir, xdir, 'npz-data/991-1000.npz'))
+
+        w = []
+        # load naive weights
+        w.append(np.load(data_files[0]))
+        for i in range(1,4): # load other weights
+            data = np.load(data_files[i])
+            w.append(data['tv1.postweights'][99])
+
+        for i in range(4):
+            plt.figure()
+            G =  nx.from_numpy_array(w[i],create_using=nx.DiGraph)
+            Ge = nx.from_numpy_array(w[i][0:e_end,0:e_end],create_using=nx.DiGraph)
+            Gi = nx.from_numpy_array(w[i][e_end:i_end,e_end:i_end],create_using=nx.DiGraph)
+            # plot clustering between e units
+            sns.histplot(data=np.ravel(nx.clustering(Ge)), bins=30, color='blue', label='within e units', stat='density', alpha=0.5, kde=True, edgecolor='white', linewidth=0.5, line_kws=dict(color='black', alpha=0.5, linewidth=1.5))
+            # plot clustering between i units
+            sns.histplot(data=np.ravel(nx.clustering(Gi)), bins=30, color='red', label='within i units', stat='density', alpha=0.5, kde=True, edgecolor='white', linewidth=0.5, line_kws=dict(color='black', alpha=0.5, linewidth=1.5))
+            # plot whole network clustering
+            sns.histplot(data=np.ravel(nx.clustering(G)), bins=30, color='black', label='whole network', stat='density', alpha=0.5, kde=True, edgecolor='white', linewidth=0.5, line_kws=dict(color='black', alpha=0.e, linewidth=1.5))
+            plt.xlabel('clustering for recurrent layer')
+            plt.ylabel('density')
+            plt.title(plt_string[i])
+            plt.legend()
+            plt.draw()
+            plt_name = plt_string[i]+"_clustering_dist_exp.png"
+            plt.savefig(os.path.join(savepath,exp_path,plt_name),dpi=300)
+            plt.clf()
+            plt.close()
+
 def nx_plot_clustering_over_time(savepath):
     experiments = get_experiments(data_dir, experiment_string)
     data_files = filenames(num_epochs, epochs_per_file)
