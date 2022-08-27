295e168c41cd14c2309029fa603e7293bd82db89
Chadbourne Smith <smith7@uchicago.edu>
2022-08-27 14:18:23 -0500

    Review `calculate_lr.py`

--- utils/extools/calculate_lr.py
+++ utils/extools/calculate_lr.py
@@ -1,27 +1,30 @@
-# Script for calculating changing learning rates across epochs from Adam Optimizer
+"""Get changes in the Adam Optimizer's learning rate across epochs."""
 
-import sys
 import numpy as np
-import matplotlib.pyplot as plt
-import h5py
 import os
 
 data_dir = "/data/experiments/"
 experiment = "fwd-pipeline-inputspikeregen-newl23-owerlr-runlonger"
 epochs_per_file = 10
 epoch_of_interest = "1-10.npz"
-batch_of_interest = 1
 savepath = "/data/results/Adam_lrs.png"
 init_main_lr = 0.001
 init_out_lr = 0.00001
 
 
-def single_epoch_lr(experiment, epoch_of_interest, batch_of_interest):
-    # determine learning rates for variables over a single epoch
+# [!] This function is incorrect. "batch of interest" is actually
+#     "epoch of interest", as the "epoch of interest" variable is a
+#     file containing data for multiple epochs
+def single_epoch_lr(experiment, epoch_of_interest, batch_of_interest=1):
+    """Get learning rate data from a single epoch."""
+
+    # Fetch data
     datapath = os.path.join(
         data_dir, experiment, "npz-data", epoch_of_interest
     )
     data = np.load(datapath)
+
+    # Unpack data
     new_in_w = data["tv0.postweights"][batch_of_interest]
     new_main_w = data["tv1.postweights"][batch_of_interest]
     new_out_w = data["tv2.postweights"][batch_of_interest]
@@ -31,6 +34,8 @@ def single_epoch_lr(experiment, epoch_of_interest, batch_of_interest):
     old_in_w = data["tv0.postweights"][batch_of_interest - 1]
     old_main_w = data["tv1.postweights"][batch_of_interest - 1]
     old_out_w = data["tv2.postweights"][batch_of_interest - 1]
+
+    # Calculate learning rate values
     in_lr = np.divide(
         np.abs(new_in_w - old_in_w),
         np.abs(in_grad),
