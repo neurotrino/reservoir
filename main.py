a7a9fa18a35f69f2222be9002907c67d873baf3d
neurotrino <neurotrino@gmail.com>
2023-03-20 15:57:14 -0500

    don't include ipynb in import

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ for filepath in Path(".").rglob("*.py"):
     """
     fp_str = str(filepath)
 
-    if fp_str in [__file__, "__init__.py", "setup.py", "ipynb_checkpoints"]:
+    if fp_str in [__file__, "__init__.py", "setup.py"] or "ipynb" in fp_str:
         continue
 
     # Convert filepaths to module paths
