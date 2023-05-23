fd8a475234afaac91ca76e6d0eab7c196261f64d
neurotrino <neurotrino@gmail.com>
2023-05-23 16:51:27 -0500

    unindent

--- utils/extools/collect_final.py
+++ utils/extools/collect_final.py
@@ -705,8 +705,8 @@ def losses_over_training(exp_dirs=spec_nointoout_dirs,exp_season='spring'):
 
     for xdir in exp_data_dirs: # loop through experiments
         np_dir = os.path.join(data_dir, xdir, "npz-data")
-        exp_path = xdir[-9:-1]
-        if exp_path!='06.03.22': # do not include that one awful experiment
+
+        if not '06.03.22' in np_dir: # do not include that one awful experiment
 
             task_loss = []
             rate_loss = []
@@ -726,31 +726,31 @@ def losses_over_training(exp_dirs=spec_nointoout_dirs,exp_season='spring'):
                 rate_losses = np.vstack([rate_losses,rate_loss])
                 task_losses = np.vstack([task_losses,task_loss])
 
-        fig, ax = plt.subplots(nrows=2, ncols=1)
-        epochs=np.arange(0,100)
-        #ax[0].plot(epochs,task_loss,label='task loss',color='teal')
-        #ax[0].plot(epochs,rate_loss,label='rate loss',color='blueviolet')
-        ax[0].plot(epochs,np.mean(task_losses,0),label='task loss',color='teal')
-        ax[0].fill_between(epochs,np.mean(task_losses,0)-np.std(task_losses,0),np.mean(task_losses,0)+np.std(task_losses,0),facecolor='mediumturquoise',alpha=0.4)
-        ax[0].plot(epochs,np.mean(rate_losses,0),label='rate loss',color='blueviolet')
-        ax[0].fill_between(epochs,np.mean(rate_losses,0)-np.std(rate_losses,0),np.mean(rate_losses,0)+np.std(rate_losses,0),facecolor='mediumslateblue',alpha=0.4)
+    fig, ax = plt.subplots(nrows=2, ncols=1)
+    epochs=np.arange(0,100)
+    #ax[0].plot(epochs,task_loss,label='task loss',color='teal')
+    #ax[0].plot(epochs,rate_loss,label='rate loss',color='blueviolet')
+    ax[0].plot(epochs,np.mean(task_losses,0),label='task loss',color='teal')
+    ax[0].fill_between(epochs,np.mean(task_losses,0)-np.std(task_losses,0),np.mean(task_losses,0)+np.std(task_losses,0),facecolor='mediumturquoise',alpha=0.4)
+    ax[0].plot(epochs,np.mean(rate_losses,0),label='rate loss',color='blueviolet')
+    ax[0].fill_between(epochs,np.mean(rate_losses,0)-np.std(rate_losses,0),np.mean(rate_losses,0)+np.std(rate_losses,0),facecolor='mediumslateblue',alpha=0.4)
 
-        for j in range(0,len(ax)):
-            ax[j].set_ylabel('loss',fontname='Ubuntu')
-            ax[j].set_xlabel('training epoch',fontname='Ubuntu')
-            ax[j].legend(prop={"family":"Ubuntu"})
-            for tick in ax[j].get_xticklabels():
-                tick.set_fontname("Ubuntu")
-            for tick in ax[j].get_yticklabels():
-                tick.set_fontname("Ubuntu")
-        plt.suptitle('Evolution of loss over training',fontname='Ubuntu')
-        plt.draw()
+    for j in range(0,len(ax)):
+        ax[j].set_ylabel('loss',fontname='Ubuntu')
+        ax[j].set_xlabel('training epoch',fontname='Ubuntu')
+        ax[j].legend(prop={"family":"Ubuntu"})
+        for tick in ax[j].get_xticklabels():
+            tick.set_fontname("Ubuntu")
+        for tick in ax[j].get_yticklabels():
+            tick.set_fontname("Ubuntu")
+    plt.suptitle('Evolution of loss over training',fontname='Ubuntu')
+    plt.draw()
 
-        save_fname = spath+'/losses_over_training.png'
-        plt.savefig(save_fname,dpi=300)
-        # Teardown
-        plt.clf()
-        plt.close()
+    save_fname = spath+'/losses_over_training.png'
+    plt.savefig(save_fname,dpi=300)
+    # Teardown
+    plt.clf()
+    plt.close()
 
 
 def demo_input_spikes_output(exp_dirs=all_save_inz_dirs,exp_season='spring'):
