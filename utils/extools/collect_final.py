61b43e438e484a22321d6264ca0521bb8aba6162
neurotrino <neurotrino@gmail.com>
2023-05-25 10:41:27 -0500

    try hstack

--- utils/extools/collect_final.py
+++ utils/extools/collect_final.py
@@ -726,11 +726,11 @@ def losses_over_training(exp_dirs=all_spring_dual_dirs,exp_season='spring'):
 
                 # concat losses together
                 if not 'rate_losses' in locals():
-                    rate_losses = rate_loss
-                    task_losses = task_loss
+                    rate_losses = np.transpose(rate_loss)
+                    task_losses = np.transpose(task_loss)
                 else:
-                    rate_losses = np.vstack([rate_losses,rate_loss])
-                    task_losses = np.vstack([task_losses,task_loss])
+                    rate_losses = np.hstack([rate_losses,np.transpose(rate_loss)])
+                    task_losses = np.hstack([task_losses,np.transpose(task_loss)])
 
                 print(np.shape(rate_losses))
 
@@ -738,10 +738,10 @@ def losses_over_training(exp_dirs=all_spring_dual_dirs,exp_season='spring'):
             epochs=np.arange(0,len(rate_losses))
             #ax[0].plot(epochs,task_loss,label='task loss',color='teal')
             #ax[0].plot(epochs,rate_loss,label='rate loss',color='blueviolet')
-            ax[0].plot(epochs,np.mean(task_losses,1),label='task loss',color='orangered')
-            ax[0].fill_between(epochs,np.mean(task_losses,1)-np.std(task_losses,1),np.mean(task_losses,1)+np.std(task_losses,1),facecolor='orangered',alpha=0.4)
-            ax[0].plot(epochs,np.mean(rate_losses,1),label='rate loss',color='darkorange')
-            ax[0].fill_between(epochs,np.mean(rate_losses,1)-np.std(rate_losses,1),np.mean(rate_losses,1)+np.std(rate_losses,1),facecolor='darkorange',alpha=0.4)
+            ax[0].plot(epochs,np.mean(task_losses,0),label='task loss',color='orangered')
+            ax[0].fill_between(epochs,np.mean(task_losses,0)-np.std(task_losses,0),np.mean(task_losses,0)+np.std(task_losses,0),facecolor='orangered',alpha=0.4)
+            ax[0].plot(epochs,np.mean(rate_losses,0),label='rate loss',color='darkorange')
+            ax[0].fill_between(epochs,np.mean(rate_losses,0)-np.std(rate_losses,0),np.mean(rate_losses,0)+np.std(rate_losses,0),facecolor='darkorange',alpha=0.4)
 
             for j in range(0,len(ax)):
                 ax[j].set_ylabel('loss',fontname='Ubuntu')
